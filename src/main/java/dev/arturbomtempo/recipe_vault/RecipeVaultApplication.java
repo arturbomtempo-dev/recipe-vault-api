93f0dcacb52f363752da7b3c7261f45cf706cc3a
Artur Bomtempo Colen <abcolen@sga.pucminas.br>
2026-02-11 23:53:27 -0300

    feat: Initialize Gradle wrapper and project structure

---
+++ src/main/java/dev/arturbomtempo/recipe_vault/RecipeVaultApplication.java
@@ -0,0 +1,13 @@
+package dev.arturbomtempo.recipe_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arturbomtempo/recipe_vault/RecipeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arturbomtempo.recipe_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
